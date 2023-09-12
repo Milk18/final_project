9b0a24cbf4ae2a2b51d1b96578033069961d592b
Milk18 <130401809+Milk18@users.noreply.github.com>
2023-09-12 17:16:17 +0300

    Update main.py

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # Connect to your MongoDB instance
-client = MongoClient('mongodb://35.195.156.191:27017/')
+client = MongoClient('mongodb://mongodb:27017/')
 db = client['project']
 collection = db['urls']
 
