a7c79961535c04f5b91e5ab83f0184b0bcb57b24
Ori <milk49@walla.co.il>
2023-09-14 13:31:24 +0300

    made the html pretty and added password for links

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -8,15 +8,25 @@ client = MongoClient('mongodb://mongodb:27017/')
 db = client['project']
 collection = db['urls']
 
+add_password = "ori"
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         text = request.form['text']
         url = request.form['url']
+        provided_password = request.form['password']
 
-        # Insert the new key-value pair into MongoDB
-        collection.insert_one({'text': text, 'url': url})
+        if provided_password == add_password:
+            # Check for duplicates before inserting
+            existing_link = collection.find_one({'$or': [{'text': text}, {'url': url}]})
+            if existing_link is None:
+                # Insert the new key-value pair into MongoDB
+                collection.insert_one({'text': text, 'url': url})
+        else:
+            return "not the password"
+
     urls = list(collection.find())  # Retrieve all key-value pairs from MongoDB
     return render_template('index2.html', urls=urls)
 
